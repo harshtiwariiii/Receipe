5d572a6ff729d1201723e4ee3713b0a502edda9a
harshtiwariii <harshtiwariii01032004@gmail.com>
2025-07-09 13:18:39 +0530

    minor changes

--- vege/models.py
+++ vege/models.py
@@ -10,4 +10,4 @@ class Receipe(models.Model):
     receipe_name= models.CharField( max_length=100)
     receipe_discription=models.TextField(null=True, blank=True)
     receipe_image=models.ImageField((""), upload_to="receipe", height_field=None, width_field=None, max_length=None)
-
+    receipe_view_count =models.IntegerField(default=1)
